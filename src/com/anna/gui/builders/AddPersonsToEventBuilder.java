22a426bb89f80c93905f35ce15856b27f4cfc683
Igor Dumchykov <multiplexor88@ukr.net>
2015-12-25 14:00:37 +0800

    Adds AddPersonsToEventBuilder

---
+++ src/com/anna/gui/builders/AddPersonsToEventBuilder.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.anna.gui.builders;
+
+import com.anna.data.Hobby;
+import com.anna.data.Person;
+import com.anna.gui.commands.ListButtonCommand;
+import com.anna.gui.commands.SimpleButtonCommand;
+import com.anna.gui.controllers.DataLoader;
+import com.anna.gui.controllers.ListDataTableController;
+import com.anna.gui.controllers.PersonController;
+import com.anna.gui.interfaces.AbstractController;
+import com.anna.gui.interfaces.Builder;
+import com.anna.gui.interfaces.ButtonCommand;
+import com.anna.gui.tables.TableFactory;
+
+/**
+ *
+ * @author igor
+ */
+public class AddPersonsToEventBuilder implements Builder
+{
+    @Override
+    public AbstractController build() 
+    {
+        ListDataTableController mainController = (ListDataTableController) DataLoader.getInstance().loadController(ListDataTableController.class, "../fxml/ListDataTable.fxml");
+        
+        mainController.getEditBtn().setDisable(true);
+        mainController.setTable(TableFactory.create(TableFactory.TableType.PERSONS_FLP));
+        
+        ButtonCommand displayDataBasePersonsCommand = new ListButtonCommand(DataLoader.getLangResources().getString("key.persons.title"));
+        ListDataTableController displayDataBasePersonsController = (ListDataTableController) DataLoader.getInstance().loadController(ListDataTableController.class, "../fxml/ListDataTable.fxml");
+        displayDataBasePersonsController.getEditBtn().setDisable(true);
+        displayDataBasePersonsController.getOkBtn().setVisible(true);
+        displayDataBasePersonsController.getCancelBtn().setVisible(true);
+        displayDataBasePersonsController.setParentController(mainController);
+        
+        displayDataBasePersonsController.setTable(TableFactory.create(TableFactory.TableType.PERSONS_FLP));
+        displayDataBasePersonsController.setData(DataLoader.getDataBaseService().getPersonService().getRepository().findAll());
+        
+        ButtonCommand<Hobby> command = new SimpleButtonCommand(DataLoader.getLangResources().getString("key.person.title"), Person.class);
+        AbstractController controller = DataLoader.getInstance().loadController(PersonController.class, "../fxml/Person.fxml");
+        command.setController(controller);
+        displayDataBasePersonsController.setCommand(command);
+
+        /**
+         * see ListDataTableController::isOnOkBySelection
+         */
+        displayDataBasePersonsController.setOnOkBySelection(true);
+        displayDataBasePersonsCommand.setController(displayDataBasePersonsController);       
+        mainController.setCommand(displayDataBasePersonsCommand);
+        
+        mainController.code = 0;
+        displayDataBasePersonsController.code = 1;
+        controller.code = 2;
+        
+        return mainController;
+    }
+}
